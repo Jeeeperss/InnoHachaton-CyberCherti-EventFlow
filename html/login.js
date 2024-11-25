cf4047022595f8e861a1342935087254bb15b76a
Foxerfob <rrr1235674@gmail.com>
2024-11-25 16:31:30 +0300

    First commit

---
+++ html/login.js
@@ -0,0 +1,9 @@
+const animateMenu = document.getElementById('animateMenu');
+
+function toLogin() {
+  animateMenu.style.animation = `toLogin 2s forwards`;  
+}
+
+function toRegister() {
+  animateMenu.style.animation = `toRegister 2s forwards`;  
+}
