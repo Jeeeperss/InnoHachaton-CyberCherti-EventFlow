a40301f25e653f675f2546ec0aabc0526dd2357c
VladDashVS <vladislavdashkevich06@gmail.com>
2024-11-26 21:41:25 +0300

    Adding Decoration Elements to Frontend.  Added design to roomList

--- html/roomList.js
+++ html/roomList.js
@@ -1,5 +1,12 @@
 const roomList = document.getElementById('roomList')
 
+document.querySelector('.dropdown-toggle').addEventListener('click', function () {
+  const dropdown = document.querySelector('.dropdown');
+  dropdown.classList.toggle('open');
+});
+
+
+
 async function logout(){
   const api = await import("./api.js")
   const token = await api.getToken()
@@ -13,8 +20,14 @@ async function logout(){
 let Rooms = []
 
 async function updateRooms() {
-  const api = await import("./api.js")
-  Rooms = await api.getRooms()
+  /*const api = await import("./api.js")
+  Rooms = await api.getRooms()*/
+  Rooms.push({
+    //temp example
+    "id":0,
+    "opening_time": "2024-11-26 14:15",
+    "is_active": true
+  })
   Rooms.push({
     //temp example
     "id":0,
@@ -26,8 +39,24 @@ async function updateRooms() {
     "opening_time": "2024-11-26 14:15",
     "is_active": false
   })
+  Rooms.push({
+    "id":0,
+    "opening_time": "2024-11-26 14:15",
+    "is_active": false
+  })
 
-  
+  Rooms.forEach(room => {
+    const roomDiv = document.createElement('div');
+    roomDiv.className = `room ${room.is_active ? 'active' : 'inactive'} ${room.private ? 'private' : ''}`;
+    // Opening Time
+    const roomTime = document.createElement('div');
+    roomTime.className = 'roomTime';
+    roomTime.innerText = `Open: ${room.opening_time}`;
+    roomDiv.appendChild(roomTime);
+
+    roomList.appendChild(roomDiv);
+});
+
   for(let i = 0; i < Rooms.length; i++){
     const roomDiv = document.createElement('div');
     const idDiv = document.createElement('div');
@@ -50,14 +79,6 @@ async function updateRooms() {
     timeDiv.className = "roomTime";
     statusDiv.className = "roomStatus";
     typeDiv.className = "roomType";
-    
-    roomList.appendChild(roomDiv);
-    roomDiv.appendChild(idDiv)
-    roomDiv.appendChild(timeDiv)
-    roomDiv.appendChild(statusDiv)
-    roomDiv.appendChild(typeDiv)
-
-     
   }
 }
 
