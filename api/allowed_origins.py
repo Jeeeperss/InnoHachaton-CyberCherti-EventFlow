9457d95749edfe5fa5c0324e5efc506ac859d465
Jeeeperss <anatoliykuragin.litra@proton.me>
2024-11-27 01:25:15 +0300

    Move CORS template

---
+++ api/allowed_origins.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+from typing import List
+
+class Origins(BaseSettings):
+    allowed_origins: List[str] = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost",
+        "http://127.0.0.1"
+    ]
+
+origins = Origins()
