c4f6123551c89999edc64416f09ea196cd2d2e63
Foxerfob <rrr1235674@gmail.com>
2024-11-28 22:13:22 +0300

    JS Chat logic.

--- html/room/chat.js
+++ html/room/chat.js
@@ -1,9 +1,12 @@
 async function startWebSocket() {
   const settings = await import("../modules/settings.js")
+  const api = await import("../modules/api.js")
   const ws_server = await settings.ws_server
 
   const params = new URLSearchParams(window.location.search);
   const roomId = Number(params.get('id'));
+  const token = await api.getToken() 
+  const me = await api.getMe(token)
 
   const wsUrl = `${ws_server}/chat/ws/${roomId}`;
 
@@ -41,10 +44,10 @@ async function startWebSocket() {
     const message = messageInput.value.trim();
     if (message) {
         const payload = {
-            sender_id: 1, // Замените на реальный ID пользователя
-            content: message
+          sender_id: me.id, // Замените на реальный ID пользователя
+          content: message,
+          email: me.email // <--<--
         };
-
         // Сериализация объекта в JSON
         socket.send(JSON.stringify(payload));
         messageInput.value = ''; // Очистка поля ввода
