c61e31a80b9bb0a6f688d27e0c18ee3fb0bcf1c3
Foxerfob <rrr1235674@gmail.com>
2024-11-28 18:59:07 +0300

    Added WebSocket chat to frontend

---
+++ html/room/chat.js
@@ -0,0 +1,57 @@
+async function startWebSocket() {
+  const settings = await import("../modules/settings.js")
+  const ws_server = await settings.ws_server
+
+  const params = new URLSearchParams(window.location.search);
+  const roomId = Number(params.get('id'));
+
+  const wsUrl = `${ws_server}/chat/ws/${roomId}`;
+
+  const messagesDiv = document.getElementById('messages');
+  const messageInput = document.getElementById('messageInput');
+  const sendButton = document.getElementById('sendButton');
+
+  // Подключение к WebSocket
+  console.log(wsUrl)
+  const socket = new WebSocket(wsUrl);
+  console.log(socket)
+
+  // Открытие соединения
+  socket.onopen = () => {
+      console.log('Connected to WebSocket');
+  };
+
+  // Получение сообщения
+  socket.onmessage = (event) => {
+      const message = document.createElement('div');
+      message.textContent = event.data;
+      messagesDiv.appendChild(message);
+  };
+
+  // Обработка ошибок
+  socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+  };
+
+  // Закрытие соединения
+  socket.onclose = () => {
+      console.log('WebSocket connection closed');
+  };
+
+  // Отправка сообщения
+  sendButton.addEventListener('click', () => {
+    const message = messageInput.value.trim();
+    if (message) {
+        const payload = {
+            sender_id: 1, // Замените на реальный ID пользователя
+            content: message
+        };
+
+        // Сериализация объекта в JSON
+        socket.send(JSON.stringify(payload));
+        messageInput.value = ''; // Очистка поля ввода
+    }
+  });
+}
+
+startWebSocket()
